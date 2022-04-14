a7e7409ae488e12bf6e289a40a3303a8d25acc12
x21179930 <100784641+x21179930@users.noreply.github.com>
2022-04-14 17:25:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function large() {
+    var array = [8 , 9, 12, 44, 56, 7, 17, 66];
+    var largest= 0;
+    
+    for (i=0; i<=largest;i++){
+        if (array[i]>largest) {
+            var largest=array[i];
+        }
+    }
+    
+    
+    
+    
+    prompt(largest);
+}
